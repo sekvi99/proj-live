69e10858377a20d79c8f99b3508431681ca6b43b
filmatkozlik@gmail.com <84912722+sekvi99@users.noreply.github.com>
2023-01-02 19:35:42 +0100

    Adding dynamic linking

--- proj_inz/graphsApp/urls.py
+++ proj_inz/graphsApp/urls.py
@@ -5,4 +5,5 @@ app_name = 'graphs_App'
 urlpatterns = [
     path('', views.home, name='home'),
     path('authors', views.about_authors, name='authors'),
+    path('data/<str:tablename>/<str:symbol>', views.data_view, name='data')
 ]
--- proj_inz/graphsApp/views.py
+++ proj_inz/graphsApp/views.py
@@ -9,6 +9,7 @@ from .models import CryptoCurrencies, StockExchange, CurrencyRates
 from .utils import get_unique_names_of_symbol_for_passed_model, calculate_percent_diffrence, get_model_by_name
 from django.contrib.contenttypes.models import ContentType
 from typing import Final
+from django.forms.models import model_to_dict
 # Create your views here.
 # ! test - view to check wheth er login works porpperly
 
@@ -63,35 +64,45 @@ def about_authors(request):
     return render(request, 'graphsApp/about.html', context)
 
 @login_required(login_url='/login/')
-def data_view(request):
+def data_view(request, tablename: str, symbol: str):
     situation_message = None # ! Tutaj zaleznie od tego czy cena rosnie czy maleje nalezy dac stosowny komunikat
     currency_description = None  # ! Tutaj opis dla kazdej waluty, ktora posiadamy (mozna po slowniku)
     form = calculatorForm() # ! Tutaj klasa formularza
-    data_name = request.GET('q') if request.GET('q') != None else ''
+    try:
+        data_model = get_model_by_name(tablename)
+        symbol = symbol.capitalize()
+        data = data_model.objects.filter(symbol=symbol).last()
+        dict_data = model_to_dict(data)
+        print(dict_data)
+        
+    except Exception as e:
+        print(str(e))
+    #data_name = request.GET('q') if request.GET('q') != None else ''
     currency = None
     #  data_name.objects.filter(symbol__icontains = '').last()
     
-    if request.method == 'POST':
-        try:
-            form = calculatorForm(data = request.POST)
-            if form.is_valid():
-                value = float(form.data['value'])
-                currency_in = form.data['currency_in']
-                """
-                Function takes:
-                1. Currency_in => Type of currency to calculate,
-                2. Value => Value from form on page,
-                3. Currency => Name of model
-                """
-                recalculate_value(currency_in, value, currency)
-        except Exception as e:
-            print(str(e))
+    # if request.method == 'POST':
+    #     try:
+    #         form = calculatorForm(data = request.POST)
+    #         if form.is_valid():
+    #             value = float(form.data['value'])
+    #             currency_in = form.data['currency_in']
+    #             """
+    #             Function takes:
+    #             1. Currency_in => Type of currency to calculate,
+    #             2. Value => Value from form on page,
+    #             3. Currency => Name of model
+    #             """
+    #             recalculate_value(currency_in, value, currency)
+    #     except Exception as e:
+    #         print(str(e))
     
     context = dict({
-        'table_name' : data_name,
+        'table_name' : symbol,
+        'data':dict_data,
         'sitution_message' : situation_message,
         'currency_description': currency_description,
         'form':form,
     })
-    return render(request, 'graphsApp/dataView.html', context)
+    return render(request, 'graphsApp/detailsPage.html', context)
     
